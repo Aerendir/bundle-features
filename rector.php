11ec9bb6cf3592e1ce451c0e81e042f2b1c2734b
Aerendir <hello@aerendir.me>
2020-08-06 16:15:55 +0200

    Update PHPStan and Rector and its config.

--- rector.php
+++ rector.php
@@ -18,9 +18,12 @@ use Rector\Set\ValueObject\SetList;
 return static function (ContainerConfigurator $containerConfigurator) : void {
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set(Option::PHP_VERSION_FEATURES, '7.2');
+    $parameters->set(Option::PHP_VERSION_FEATURES, '7.3');
 
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests'
+    ]);
 
     $parameters->set(Option::AUTOLOAD_PATHS, [__DIR__ . '/vendor-bin/phpunit/vendor/autoload.php']);
 
@@ -42,7 +45,6 @@ return static function (ContainerConfigurator $containerConfigurator) : void {
             SetList::FRAMEWORK_EXTRA_BUNDLE_40,
             SetList::FRAMEWORK_EXTRA_BUNDLE_50,
             SetList::GUZZLE_50,
-            SetList::NAMING,
             SetList::PERFORMANCE,
             SetList::PHP_52,
             SetList::PHP_53,
@@ -51,6 +53,7 @@ return static function (ContainerConfigurator $containerConfigurator) : void {
             SetList::PHP_70,
             SetList::PHP_71,
             SetList::PHP_72,
+            SetList::PHP_73,
             SetList::PHPSTAN,
             SetList::PHPUNIT_40,
             SetList::PHPUNIT_50,
@@ -66,7 +69,6 @@ return static function (ContainerConfigurator $containerConfigurator) : void {
             SetList::PHPUNIT_SPECIFIC_METHOD,
             SetList::PHPUNIT_YIELD_DATA_PROVIDER,
             SetList::UNWRAP_COMPAT,
-            SetList::PRIVATIZATION,
             SetList::SOLID,
             SetList::SYMFONY_26,
             SetList::SYMFONY_28,
@@ -81,7 +83,7 @@ return static function (ContainerConfigurator $containerConfigurator) : void {
             SetList::SYMFONY_43,
             SetList::SYMFONY_44,
             SetList::SYMFONY_50,
-            SetList::SYMFONY50_TYPES,
+            SetList::SYMFONY_50_TYPES,
             SetList::SYMFONY_CODE_QUALITY,
             SetList::SYMFONY_CONSTRUCTOR_INJECTION,
             SetList::SYMFONY_PHPUNIT,
@@ -97,35 +99,29 @@ return static function (ContainerConfigurator $containerConfigurator) : void {
         ]
     );
 
-    $parameters->set(Option::IMPORT_SHORT_CLASSES_PARAMETER, false);
-
     $parameters->set(
         Option::EXCLUDE_RECTORS,
         [
-            Rector\TypeDeclaration\Rector\ClassMethod\AddArrayReturnDocTypeRector::class,
-            Rector\TypeDeclaration\Rector\ClassMethod\AddArrayParamDocTypeRector::class,
+            Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector::class,
+            Rector\CodeQuality\Rector\Concat\JoinStringConcatRector::class,
+            Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector::class,
+            Rector\CodingStyle\Rector\Class_\AddArrayDefaultToArrayPropertyRector::class,
+            Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector::class,
+            Rector\CodingStyle\Rector\ClassMethod\RemoveDoubleUnderscoreInMethodNameRector::class,
+            Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class,
+            Rector\CodingStyle\Rector\Identical\IdenticalFalseToBooleanNotRector::class,
+            Rector\CodingStyle\Rector\Switch_\BinarySwitchToIfElseRector::class,
+            Rector\CodingStyle\Rector\Throw_\AnnotateThrowablesRector::class,
+            Rector\CodingStyle\Rector\Use_\RemoveUnusedAliasRector::class,
+            Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector::class, // Maybe good one day
+            Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector::class,
+            Rector\PHPUnit\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector::class,
+            Rector\Privatization\Rector\ClassMethod\PrivatizeLocalOnlyMethodRector::class,
+            Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector::class,
             Rector\SOLID\Rector\ClassMethod\UseInterfaceOverImplementationInConstructorRector::class,
             Rector\SOLID\Rector\Property\AddFalseDefaultToBoolPropertyRector::class,
-            Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector::class,
-            Rector\Privatization\Rector\ClassMethod\PrivatizeLocalOnlyMethodRector::class,
-            Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector::class, // Maybe good one day
-            Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector::class,
-            Rector\DeadCode\Rector\Class_\RemoveUnusedClassesRector::class,
-            Rector\CodingStyle\Rector\Use_\RemoveUnusedAliasRector::class,
-            Rector\CodingStyle\Rector\Throw_\AnnotateThrowablesRector::class,
-            Rector\CodingStyle\Rector\Switch_\BinarySwitchToIfElseRector::class,
-            Rector\CodingStyle\Rector\Identical\IdenticalFalseToBooleanNotRector::class,
-            Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class,
-            Rector\CodingStyle\Rector\ClassMethod\RemoveDoubleUnderscoreInMethodNameRector::class,
-            Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector::class,
-            Rector\CodingStyle\Rector\Class_\AddArrayDefaultToArrayPropertyRector::class,
-            Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector::class,
-            Rector\CodeQuality\Rector\Concat\JoinStringConcatRector::class,
-            Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector::class,
-
-            // Bugged
-            Rector\TypeDeclaration\Rector\Property\PropertyTypeDeclarationRector::class,
-            Rector\PHPUnit\Rector\MethodCall\GetMockBuilderGetMockToCreateMockRector::class,
+            Rector\TypeDeclaration\Rector\ClassMethod\AddArrayParamDocTypeRector::class,
+            Rector\TypeDeclaration\Rector\ClassMethod\AddArrayReturnDocTypeRector::class,
         ]
     );
 };
